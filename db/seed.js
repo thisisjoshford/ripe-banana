0d382a33518ea0c29ed791b195d57aad428b9d3a
Josh Ford <thisisjoshford@gmail.com>
2020-03-26 15:26:59 -0700

    seed data working!

--- db/seed.js
+++ db/seed.js
@@ -1,23 +1,16 @@
-const Book = require('../models/Book');
+const Studio = require('../lib/models/studio');
 const chance = require('chance').Chance();
 
-// specifying the number of tweets to create with our seed function
-module.exports = async({ booksToCreate = 100 } = {}) => {
-  // creating tweets
-  // creating an array of tweetsToCreate length
-  // map through the array
-  // -> for each item in the array we create an object with { handle, text }
-  // for each tweet in the mapped array we create a tweet in our mongodb
-  const genre = ['fiction', 'non-fiction'];
-  const books = await Book.create([...Array(booksToCreate)].map(() => ({
-    author: chance.name(),
-    title: chance.animal() + ' ' + chance.word(),
-    genre: chance.pickone(genre)
-  })));
+module.exports = async({ studiosToCreate = 10 } = {}) => {
 
-  // await Comment.create([...Array(commentsToCreate)].map(() => ({
-  //   tweetId: chance.pickone(tweets)._id,
-  //   handle: chance.pickone(handles),
-  //   text: chance.sentence()
-  // })));
+  const country = ['Afghanistan',  'Albania',  'Algeria',  'Andorra',  'Angola',  'Anguilla',  'Barbuda',  'Argentina',  'Armenia',  'Aruba',  'Australia',  'Austria',  'Azerbaijan',  'Bahamas',  'Bahrain',  'Bangladesh', 'Barbados',  'Belarus',  'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', ' Tajikistan', ' Tanzania', 'Thailand', 'Timor', 'Este', 'Togo', 'Tonga', 'Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'];
+
+  const studio = await Studio.create([...Array(studiosToCreate)].map(() => ({
+    name: chance.animal() + ' ' + 'Studios', 
+    address: {
+      city: chance.city(),
+      state: chance.state(),
+      country: chance.pickone(country)
+    }
+  })));
 };
