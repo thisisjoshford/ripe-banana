46e4ae21b97156cf8f84ce93076f85b5852debfc
Josh Ford <thisisjoshford@gmail.com>
2020-03-27 10:11:31 -0700

    film route

---
+++ __tests__/film.test.js
@@ -0,0 +1,26 @@
+const { getActor, getActors } = require('../db/data-helpers');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('app routes', () => {
+
+  it('creates a film', () => {
+    return request(app)
+      .post('/api/v1/film')
+      .send({
+        name: 'Mr. Bean',
+        dob: '02/19/1982',
+        pob: 'Milwaukee, WI'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Mr. Bean',
+          dob: expect.any(String),
+          pob: 'Milwaukee, WI',
+          __v: 0
+        });
+      });
+  });
+});
