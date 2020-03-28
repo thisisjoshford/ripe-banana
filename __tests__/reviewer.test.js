96fd0bd38ba87f1f10a3e254111d9f851b259ac9
Josh Ford <thisisjoshford@gmail.com>
2020-03-28 11:52:02 -0700

    create a reviewer failing test created

---
+++ __tests__/reviewer.test.js
@@ -0,0 +1,25 @@
+const { getFilm, getFilms, getStudios } = require('../db/data-helpers');
+
+const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('app routes', () => {
+
+  it('creates a reviewer', () => {
+    return request(app)
+      .post('/api/v1/reviewer')
+      .send({
+        name: 'Jay Sherman',
+        company: 'Coming Attractions'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Jay Sherman',
+          company: 'Coming Attractions',
+          __v: 0 
+        });
+      });
+  });  
+});
