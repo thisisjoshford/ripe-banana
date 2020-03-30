d868d309c5a47944bd1b03d341dc8077f3a71ce2
Josh Ford <thisisjoshford@gmail.com>
2020-03-29 21:30:44 -0700

    review seed data

--- db/seed.js
+++ db/seed.js
@@ -2,10 +2,11 @@ const Studio = require('../lib/models/studio');
 const Film = require('../lib/models/film');
 const Actor = require('../lib/models/actor');
 const Reviewer = require('../lib/models/reviewer');
+const Reviews = require('../lib/models/reviews');
 const chance = require('chance').Chance();
 const moment = require('moment');
 
-module.exports = async({ studiosToCreate = 10, actorsToCreate = 10, filmsToCreate = 10 } = {}) => {
+module.exports = async({ studiosToCreate = 10, actorsToCreate = 10, filmsToCreate = 10, reviewerToCreate = 10, reviewsToCreate = 30 } = {}) => {
 
   const country = ['Afghanistan',  'Albania',  'Algeria',  'Andorra',  'Angola',  'Anguilla',  'Barbuda',  'Argentina',  'Armenia',  'Aruba',  'Australia',  'Austria',  'Azerbaijan',  'Bahamas',  'Bahrain',  'Bangladesh', 'Barbados',  'Belarus',  'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', ' Tajikistan', ' Tanzania', 'Thailand', 'Timor', 'Este', 'Togo', 'Tonga', 'Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'];
 
@@ -39,9 +40,17 @@ module.exports = async({ studiosToCreate = 10, actorsToCreate = 10, filmsToCreat
     }]
   })));
 
-  const reviewer = await Reviewer.create([...Array(filmsToCreate)].map(() => ({
+  const reviewer = await Reviewer.create([...Array(reviewerToCreate)].map(() => ({
     name: `${chance.prefix()} ${chance.name()}`,
     company: `${chance.weekday()} ${chance.animal()} ${chance.word()}.com`,
+  })));
+
+  const ratings = [1, 2, 3, 4, 5];
+  const reviews = await Reviews.create([...Array(reviewsToCreate)].map(() => ({
+    rating: chance.pickone(ratings),
+    reviewer: chance.pickone(reviewer._id),
+    review: chance.word(10),
+    film: chance.pickone(film._id)
   })));
 };
 
